a71a5f087637fc6cf57186768f8e51f42b3fb1a4
Carlos Ronny de Sousa <135059087+prof-ronny@users.noreply.github.com>
2025-05-16 19:19:37 +0000

    Finalizando Exemplo Express

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const lista = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Doe' }
+    ];
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+        res.send(lista)
+    }
+);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+}
+);
+
+
